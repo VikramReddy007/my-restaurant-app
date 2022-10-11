27d2bae1dfb767e35021c521900926e2f5a18e5a
Vikram Reddy <vikramreddymokalla@gmail.com>
2022-10-11 20:06:38 +0530

    Minor updates - UI

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './header.css'
-import mainLogo from './mainLogo.png'
-import titleLogo from './TitleLogo.svg'
 import '../../../styles/commonClasses.css'
 
 const Header = () => {
+  const mainLogo = "https://ik.imagekit.io/vb0g5zv8u/mainLogo_NxDxfwNIu.png?ik-sdk-version=javascript-1.4.3&updatedAt=1665498819563";
+  const titleLogo = "https://ik.imagekit.io/vb0g5zv8u/TitleLogo_lE-cNJEm1.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1665498830812";
   return (
     <>
       <div className='header max-width'>
